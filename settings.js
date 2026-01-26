e2398d034aaa8bf31f7ebce580c0ade61cfb7900
ralistichero <20u4078@student.mau.edu.ng>
2026-01-26 08:39:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
 const settings = {
-  packname: 'Werey Bot',
+  packname: 'Knight Bot',
   author: '‎',
-  botName: "Kdealz",
-  botOwner: 'your zaddy', // Your name
+  botName: "Roylon Bot",
+  botOwner: 'Kdealer', // Your name
   ownerNumber: '2348100996979', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.0.6",
+  description: "Abeg no over use am.",
+  version: "3.0.5",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
 
