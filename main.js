2ab08ef4b1e533d0d38407991dad251577bc6fa1
ralistichero <20u4078@student.mau.edu.ng>
2026-02-24 12:53:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -143,7 +143,7 @@ const { anticallCommand, readState: readAnticallState } = require('./commands/an
 const { pmblockerCommand, readState: readPmBlockerState } = require('./commands/pmblocker');
 const settingsCommand = require('./commands/settings');
 const soraCommand = require('./commands/sora');
-const hmmCommand = require('./commands/hmm');
+const hmmCommand = require('/commands/hmm');
 
 // Global settings
 global.packname = settings.packname;
