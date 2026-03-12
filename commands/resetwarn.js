264bb29594b1cc2bdee12f1125098f70341c0506
realistichero <benedictashira@gmail.com>
2026-03-12 11:57:25 +0100

    Update resetwarn.js

--- commands/resetwarn.js
+++ commands/resetwarn.js
@@ -84,7 +84,7 @@ async function resetWarnCommand(sock, chatId, senderId, mentionedJids, message) 
         fs.writeFileSync(warningsPath, JSON.stringify(warnings, null, 2));
 
         await sock.sendMessage(chatId, {
-            text: `✅ Go and sin no more @${userToReset.split('@')[0]}`,
+            text: `✅ Go and sin no more @${userToReset.split('@')}`,
             mentions: [userToReset]
         }, { quoted: message });
     } catch (error) {
